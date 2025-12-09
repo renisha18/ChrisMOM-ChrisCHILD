0aed2803c9b00adea8b0e48bf403f6550e13dfdb
star <renishaensalaita.27it@licet.ac.in>
2025-12-09 15:34:46 +0530

    db

--- index.js
+++ index.js
@@ -66,11 +66,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 db.connect()
   .then(() => console.log("Connected to Postgres"))
